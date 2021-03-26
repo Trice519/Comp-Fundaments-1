198e8494f87bb544d403aae0669897d8460b2420
Trice519 <81279853+Trice519@users.noreply.github.com>
2021-03-26 00:25:54 -0500

    Add files via upload

---
+++ HW1/BusinessCard.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {        
+    std::cout << "Bill Miller Bar-B-Q" << std::endl;
+    std::cout << "1616 N Loop 1604 E, San Antonio, TX 78232" << std::endl;  
+    std::cout << "210-545-4303" << std::endl;  
+    std::cout << "https://www.billmillerbbq.com/locations/bill-miller-47/" << std::endl;  
+    return 0;
+}
+
